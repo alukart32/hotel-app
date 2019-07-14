1ca9bfd68b7fc40e37b9311a214a76d062623ee2
Ivan_Tarasov <alukart32@gmail.com>
2019-07-14 12:50:45 +0300

    Implement createBooking method

--- ServicesModule/src/main/java/ru/relex/hotelteam/service/impl/BookingServiceImpl.java
+++ ServicesModule/src/main/java/ru/relex/hotelteam/service/impl/BookingServiceImpl.java
@@ -1,28 +1,28 @@
 package ru.relex.hotelteam.service.impl;
 
-import ru.relex.hotelteam.db.mapper.IUserMapper;
+import ru.relex.hotelteam.db.mapper.IBookingMapper;
 import ru.relex.hotelteam.service.IBookingService;
 import ru.relex.hotelteam.service.dto.domain.BookingCreateDTO;
 import ru.relex.hotelteam.service.dto.domain.BookingDTO;
 import ru.relex.hotelteam.service.dto.domain.BookingUpdateDTO;
-import ru.relex.hotelteam.service.mapstruct.IUserMapstruct;
+import ru.relex.hotelteam.service.mapstruct.IBookingMapstruct;
 
 import java.util.List;
 
 public class BookingServiceImpl implements IBookingService {
 
-    private final IUserMapper mapper;
-    private final IUserMapstruct mapstruct;
+    private final IBookingMapper mapper;
+    private final IBookingMapstruct mapstruct;
 
-    public BookingServiceImpl(final IUserMapper mapper,
-                           final IUserMapstruct mapstruct) {
+    public BookingServiceImpl(final IBookingMapper mapper,
+                           final IBookingMapstruct mapstruct) {
         this.mapper = mapper;
         this.mapstruct = mapstruct;
     }
 
     @Override
     public BookingDTO createBooking(BookingCreateDTO booking) {
-        return null;
+        return mapstruct.toDTO(mapper.createBooking(mapstruct.fromCreateDTO(booking)));
     }
 
     @Override
