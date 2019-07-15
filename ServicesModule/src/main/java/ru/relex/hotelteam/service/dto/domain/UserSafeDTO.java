5d2ffd521457ef8a98722a56850d709442dbb45a
Ivan_Tarasov <alukart32@gmail.com>
2019-07-15 12:27:46 +0300

    Change Room room to int roomId in BookingDTO

---
+++ ServicesModule/src/main/java/ru/relex/hotelteam/service/dto/domain/UserSafeDTO.java
@@ -0,0 +1,85 @@
+package ru.relex.hotelteam.service.dto.domain;
+
+import ru.relex.hotelteam.shared.model.Authority;
+
+import java.util.Date;
+
+
+/**
+ * UserSafeDTO - представляет User без пароля (для безопасности)
+ */
+public class UserSafeDTO {
+
+    private int id;
+    private String login;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String email;
+    private Date birthDate;
+    private Authority authority;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public Authority getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(Authority authority) {
+        this.authority = authority;
+    }
+}
