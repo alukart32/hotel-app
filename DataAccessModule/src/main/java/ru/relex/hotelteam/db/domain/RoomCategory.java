7d080b143971a1d645db89c1d7775bf1aa7f9947
Ivan_Tarasov <alukart32@gmail.com>
2019-07-16 13:36:48 +0000

    Add an id field to FacilityDTO
    (cherry picked from commit f58c20a8aa06312fd28d0f51698b31c0633d2fc0)

--- DataAccessModule/src/main/java/ru/relex/hotelteam/db/domain/RoomCategory.java
+++ DataAccessModule/src/main/java/ru/relex/hotelteam/db/domain/RoomCategory.java
@@ -16,6 +16,14 @@ public class RoomCategory {
         this.id = id;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
