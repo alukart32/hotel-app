be6f739605de4d010932db7901a1fe7c71ed6ebc
Anatoliy Yakimov <yakimov.tolya@gmail.com>
2019-07-18 11:26:18 +0300

    Add development configuration for database

--- DataAccessModule/src/main/java/ru/relex/hotelteam/db/configuration/DbDevConfiguration.java
+++ DataAccessModule/src/main/java/ru/relex/hotelteam/db/configuration/DbDevConfiguration.java
@@ -1,15 +1,19 @@
-package ru.relex.hotelteam.db;
-
+package ru.relex.hotelteam.db.configuration;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
+/**
+ * Author: Yakimov Date: 18.07.2019 Time: 11:17
+ */
 @Configuration
+@Profile("dev")
 @EnableTransactionManagement
-@PropertySource("db.properties")
+@PropertySource("db-dev.properties")
 @MapperScan("ru.relex.hotelteam.db.mapper")
-public class DbConfiguration {
+public class DbDevConfiguration {
+
 }
