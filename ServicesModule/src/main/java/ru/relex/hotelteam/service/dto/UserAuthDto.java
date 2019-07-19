8c449c631d900b64dbfc036fcfce0a4d1bb4567b
Ivan_Tarasov <alukart32@gmail.com>
2019-07-19 10:46:43 +0300

    Add class UserAuthDto

---
+++ ServicesModule/src/main/java/ru/relex/hotelteam/service/dto/UserAuthDto.java
@@ -0,0 +1,44 @@
+package ru.relex.hotelteam.service.dto;
+
+/**
+ * Created by Tarasov Ivan on 19/07/2019.
+ */
+public class UserAuthDto {
+
+  private int id;
+  private String login;
+  private String email;
+  private String password;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
