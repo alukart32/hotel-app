2c93de6581ebbaf9a94ca26d86b73ee719a9fb82
Ivan_Tarasov <alukart32@gmail.com>
2019-07-19 17:14:32 +0300

    Remove id from UserBaseDto

--- ServicesModule/src/main/java/ru/relex/hotelteam/service/dto/UserBaseDto.java
+++ ServicesModule/src/main/java/ru/relex/hotelteam/service/dto/UserBaseDto.java
@@ -5,7 +5,6 @@ import ru.relex.hotelteam.shared.model.Authority;
 
 public class UserBaseDto {
 
-  private int id;
   private String login;
   private String email;
   private String firstName;
@@ -13,14 +12,6 @@ public class UserBaseDto {
   private String middleName;
   private Date birthDate;
   private Authority authority;
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
 
   public String getLogin() {
     return login;
