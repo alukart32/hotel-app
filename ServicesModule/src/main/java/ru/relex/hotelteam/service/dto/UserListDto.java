616764d943523ce304a8fa526c30418b39ef8865
Ivan_Tarasov <alukart32@gmail.com>
2019-07-19 12:19:57 +0300

    Add UserListDto

---
+++ ServicesModule/src/main/java/ru/relex/hotelteam/service/dto/UserListDto.java
@@ -0,0 +1,80 @@
+package ru.relex.hotelteam.service.dto;
+
+import java.util.Date;
+import ru.relex.hotelteam.shared.model.Authority;
+
+public class UserListDto {
+
+  private int id;
+  private String login;
+  private String email;
+  private String firstName;
+  private String lastName;
+  private String middleName;
+  private Date birthDate;
+  private Authority authority;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public Authority getAuthority() {
+    return authority;
+  }
+
+  public void setAuthority(Authority authority) {
+    this.authority = authority;
+  }
+}
