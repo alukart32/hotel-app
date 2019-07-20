e263d46adcc9cdde1263b0a964c423c633c31d62
Ivan_Tarasov <alukart32@gmail.com>
2019-07-20 19:00:35 +0300

    Add BookingPayment to work with payment

---
+++ DataAccessModule/src/main/java/ru/relex/hotelteam/db/domain/BookingPayment.java
@@ -0,0 +1,56 @@
+package ru.relex.hotelteam.db.domain;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+/**
+ * Created by Tarasov Ivan on 20/07/2019
+ */
+public class BookingPayment {
+
+  private int id;
+  private int userId;
+  private int bookingId;
+  private OffsetDateTime date;
+  private BigDecimal total;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public int getBookingId() {
+    return bookingId;
+  }
+
+  public void setBookingId(int bookingId) {
+    this.bookingId = bookingId;
+  }
+
+  public OffsetDateTime getDate() {
+    return date;
+  }
+
+  public void setDate(OffsetDateTime date) {
+    this.date = date;
+  }
+
+  public BigDecimal getTotal() {
+    return total;
+  }
+
+  public void setTotal(BigDecimal total) {
+    this.total = total;
+  }
+}
