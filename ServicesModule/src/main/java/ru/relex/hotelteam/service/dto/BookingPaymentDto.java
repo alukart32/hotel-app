45412a13fe264261f31f9b3e02e39dd1cfa5a2e5
Ivan_Tarasov <alukart32@gmail.com>
2019-07-20 19:15:40 +0300

    Fill BookingPaymentDto

--- ServicesModule/src/main/java/ru/relex/hotelteam/service/dto/BookingPaymentDto.java
+++ ServicesModule/src/main/java/ru/relex/hotelteam/service/dto/BookingPaymentDto.java
@@ -1,5 +1,44 @@
 package ru.relex.hotelteam.service.dto;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
 public class BookingPaymentDto {
 
+  private int userId;
+  private int bookingId;
+  private OffsetDateTime date;
+  private BigDecimal total;
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public int getBookingId() {
+    return bookingId;
+  }
+
+  public void setBookingId(int bookingId) {
+    this.bookingId = bookingId;
+  }
+
+  public OffsetDateTime getDate() {
+    return date;
+  }
+
+  public void setDate(OffsetDateTime date) {
+    this.date = date;
+  }
+
+  public BigDecimal getTotal() {
+    return total;
+  }
+
+  public void setTotal(BigDecimal total) {
+    this.total = total;
+  }
 }
