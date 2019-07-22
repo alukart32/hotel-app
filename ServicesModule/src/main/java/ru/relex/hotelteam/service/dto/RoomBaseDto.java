06407dc13697cbfd38987dbde6442117326fcd4c
Anatoliy Yakimov <yakimov.tolya@gmail.com>
2019-07-22 14:44:50 +0300

    Add Room DTOs

--- ServicesModule/src/main/java/ru/relex/hotelteam/service/dto/RoomBaseDto.java
+++ ServicesModule/src/main/java/ru/relex/hotelteam/service/dto/RoomBaseDto.java
@@ -1,30 +1,21 @@
 package ru.relex.hotelteam.service.dto;
 
+import java.math.BigDecimal;
 import java.util.List;
 import ru.relex.hotelteam.db.domain.Facility;
-import ru.relex.hotelteam.db.domain.RoomCategory;
-import ru.relex.hotelteam.db.domain.RoomType;
 
 /**
  * Author: Yakimov Date: 20.07.2019 Time: 21:04
  */
 public class RoomBaseDto {
 
-  private int id;
   private int number;
   private int floor;
 
-  private RoomType type;
-  private RoomCategory category;
+  private BigDecimal dailyPrice;
+
+
   private List<Facility> facilities;
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
 
   public int getNumber() {
     return number;
@@ -42,28 +33,21 @@ public class RoomBaseDto {
     this.floor = floor;
   }
 
-  public RoomType getType() {
-    return type;
-  }
-
-  public void setType(RoomType type) {
-    this.type = type;
-  }
-
-  public RoomCategory getCategory() {
-    return category;
-  }
-
-  public void setCategory(RoomCategory category) {
-    this.category = category;
-  }
-
   public List<Facility> getFacilities() {
     return facilities;
   }
 
   public void setFacilities(List<Facility> facilities) {
     this.facilities = facilities;
+  }
+
+
+  public BigDecimal getDailyPrice() {
+    return dailyPrice;
+  }
+
+  public void setDailyPrice(BigDecimal dailyPrice) {
+    this.dailyPrice = dailyPrice;
   }
 
 }
---
+++ ServicesModule/src/main/java/ru/relex/hotelteam/service/dto/RoomWithIdDto.java
@@ -0,0 +1,14 @@
+package ru.relex.hotelteam.service.dto;
+
+public class RoomWithIdDto extends RoomBaseDto {
+
+  private int id;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+}
