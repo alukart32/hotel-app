fc522812c597509463e1d678f9a4e6f8a4f3c852
Ivan_Tarasov <alukart32@gmail.com>
2019-07-25 23:34:31 +0300

    Add HotelAppAuthFilter

---
+++ SecurityModule/src/main/java/ru/relex/hotelteam/security/filter/HotelAppAuthFilter.java
@@ -0,0 +1,50 @@
+package ru.relex.hotelteam.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.relex.hotelteam.security.model.LoginInfo;
+
+/**
+ * Отвечает за авторизацию пользователя при логине в систему
+ */
+public class HotelAppAuthFilter extends UsernamePasswordAuthenticationFilter {
+
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  public HotelAppAuthFilter(AuthenticationSuccessHandler successHandler,
+      AuthenticationManager authManager) {
+
+    super();
+    this.setAllowSessionCreation(false); //в REST сессии не предусмотрены
+    /*
+    стандартный спринговый менеджер аутенфикации который под капотом
+    использует всё что настроено в DaoAuthenticationProvider
+     */
+    this.setAuthenticationManager(authManager);
+    this.setFilterProcessesUrl("/login"); // всё что не логин этим фильтром игнорируется
+    this.setAuthenticationSuccessHandler(successHandler); // в случае успешной аутентификации будет вызнан этот хэндлер
+  }
+
+  @Override
+  public Authentication attemptAuthentication(final HttpServletRequest request, final HttpServletResponse response) throws AuthenticationException {
+    try {
+      //самое важное в этом методе: прочитать пользователя из тела JSON
+      var user = mapper.readValue(request.getReader(), LoginInfo.class);
+      // и отдать эту информацию на дальнейшую аутентификацию
+      return getAuthenticationManager().authenticate(
+          new UsernamePasswordAuthenticationToken(user.getLogin(), user.getPassword()));
+    } catch (IOException e) {
+      throw new BadCredentialsException("Failed to parse user login info");
+    }
+  }
+
+}
