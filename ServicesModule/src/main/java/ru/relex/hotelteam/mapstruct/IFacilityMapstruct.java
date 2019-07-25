c4d9d2f524c7e0ffe6fb546e97bb9902e1899ac7
Anatoliy Yakimov <yakimov.tolya@gmail.com>
2019-07-25 20:49:55 +0300

    Implement FacilityMapstruct

--- ServicesModule/src/main/java/ru/relex/hotelteam/mapstruct/IFacilityMapstruct.java
+++ ServicesModule/src/main/java/ru/relex/hotelteam/mapstruct/IFacilityMapstruct.java
@@ -1,17 +1,25 @@
 package ru.relex.hotelteam.mapstruct;
 
-//import ru.relex.hotelteam.db.domain.Facility;
-//import ru.relex.hotelteam.service.dto.FacilityDTO;
-//
-//import java.util.List;
 
+import java.util.List;
+import org.mapstruct.IterableMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueMappingStrategy;
+import ru.relex.hotelteam.db.domain.Facility;
+import ru.relex.hotelteam.dto.facility.FacilityDtoWithNoPrice;
+
+@Mapper(componentModel = "spring")
 public interface IFacilityMapstruct {
-//
-//    Facility fromDTO (FacilityDTO dto);
-//
-//    FacilityDTO toDTO (Facility facility);
-//
-//    List<Facility> fromDTO (List<Facility> facilityList);
-//
-//    List<FacilityDTO> toDTO (List<FacilityDTO> facilityDTOList);
+
+  @Mapping(target = "price", ignore = true)
+  Facility fromDto(FacilityDtoWithNoPrice dto);
+
+  @IterableMapping(nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+  List<Facility> fromDto(List<FacilityDtoWithNoPrice> facilityList);
+
+  FacilityDtoWithNoPrice toDto(Facility facility);
+
+  @IterableMapping(nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+  List<FacilityDtoWithNoPrice> toDto(List<Facility> facilityDTOList);
 }
