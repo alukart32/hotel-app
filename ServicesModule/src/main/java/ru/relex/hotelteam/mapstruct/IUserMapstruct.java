a997986852bec92e1f8c24409249f20805fce879
Ivan_Tarasov <alukart32@gmail.com>
2019-07-26 00:34:08 +0300

    Get basic security

--- ServicesModule/src/main/java/ru/relex/hotelteam/mapstruct/IUserMapstruct.java
+++ ServicesModule/src/main/java/ru/relex/hotelteam/mapstruct/IUserMapstruct.java
@@ -26,27 +26,32 @@ public abstract class IUserMapstruct {
     return passwordEncoder.encode(dto.getPassword());
   }
 
-  abstract User fromDto(UserDto dto);
-
   abstract List<User> fromDto(List<UserBaseDto> userDtoList);
 
   abstract UserDto toDto(User user);
 
+  @Mapping(target = "password", expression = "java(mapPassword(dto))")
+  public abstract User toDomain(UserDto dto);
+
+  public abstract List<UserBaseDto> fromDomain(List<User> users);
+
+  public abstract List<User> toDomain(List<UserDto> userDtos);
+
   @Mapping(target = "password", ignore = true)
-  abstract User fromBaseDto(UserBaseDto dto);
+  public abstract User fromBaseDto(UserBaseDto dto);
 
-  abstract UserBaseDto toBaseDto(User user);
+  public abstract UserBaseDto toBaseDto(User user);
 
-  abstract UserUpdateDto toUpdateDto(User user);
+  public abstract UserUpdateDto toUpdateDto(User user);
 
   @Mapping(target = "password", ignore = true)
   @Mapping(target = "login", ignore = true)
   @Mapping(target = "id", ignore = true)
   @Mapping(target = "email", ignore = true)
   @Mapping(target = "authority", ignore = true)
-  abstract User fromUpdateDto(UserUpdateDto userUpdateDto);
+  public abstract User fromUpdateDto(UserUpdateDto userUpdateDto);
 
-  abstract UserSecurityDto toSecurityDto(User user);
+  public abstract UserSecurityDto toSecurityDto(User user);
 
   @Mapping(target = "middleName", ignore = true)
   @Mapping(target = "lastName", ignore = true)
@@ -54,7 +59,6 @@ public abstract class IUserMapstruct {
   @Mapping(target = "firstName", ignore = true)
   @Mapping(target = "birthDate", ignore = true)
   @Mapping(target = "authority", ignore = true)
-  abstract User fromSecurityDto(UserSecurityDto userSecurityDto);
+  public abstract User fromSecurityDto(UserSecurityDto userSecurityDto);
 
-  abstract List<UserBaseDto> toDto(List<User> users);
 }
